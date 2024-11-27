91505a5ea1137453a31c18245346eb21c721472d
Harriet2004 <s3933558@student.rmit.edu.au>
2024-11-27 20:50:01 +1100

    Php file completed

--- verses.php
+++ verses.php
@@ -1,6 +1,13 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 $db = new SQLite3('BibleVerse.db');
 $query = 'SELECT * FROM BIBLE';
 $result = $db->query($query);
 $verses = [];
+while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+    $verses[] = $row;
+}
+header('Content-Type: application/json');
+echo json_encode($verses);
 ?>
