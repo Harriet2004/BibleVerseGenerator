167fa1deab14a9686fcb38cf0c8a26c75d090f51
Harriet2004 <s3933558@student.rmit.edu.au>
2024-12-05 15:35:51 +0400

    Changed the database file and sql command in php file

--- verses.php
+++ verses.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $db = new SQLite3('BibleVerse.db');
-$query = 'SELECT * FROM BIBLE';
+$query = 'SELECT * FROM VERSES';
 $result = $db->query($query);
 $verses = [];
 while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
